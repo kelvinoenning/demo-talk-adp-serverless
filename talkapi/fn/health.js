fba10d6e15f3fc99b07b027b1bfed150e8a6814b
Kelvin Stang Oenning <kelvinstang@hotmail.com>
2018-03-28 22:29:28 -0300

    update

--- talkapi/fn/health.js
+++ talkapi/fn/health.js
@@ -1,14 +1,17 @@
 'use strict';
 
+// Função que será chamada pelo Lambda;
 module.exports.check = (event, context, callback) => {
 
-  const response = {
-    statusCode: 200,
-    headers: {
+  const response = { // Objeto de resposta para a chamada HTTP.
+    statusCode: 200, // Seta o status da requisição como 200.
+    headers: { // Adiciona o pacote de headers na requisição
       "Access-Control-Allow-Origin": "*"
     }
   };
 
-  callback(null, response);
+  callback(null, response); // Envia a resposta para o API Gateway.
 
+  // Lembrando que neste caso é uma resposta para uma função de HTTP.
+  // Somente o evento do API Gateway entende (statusCode) e (headers) no objeto.
 };
